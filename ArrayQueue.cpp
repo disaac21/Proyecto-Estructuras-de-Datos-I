0ab9a1d09d8cf265a6076aa2841a5b9db0884479
Your Name <danielisaacjuarez@hmail.com>
2023-03-28 15:11:14 -0600

    AHORA SI, COMO QUEVEDO

--- ArrayQueue.cpp
+++ ArrayQueue.cpp
@@ -126,9 +126,10 @@ void ArrayQueue::imprime(){// metodo para imprimir los elementos de la cola
 }
 void ArrayQueue::anula()//metodo para borrar los elementos de la cola
 {
-    for (int i = 0; i < capacidad; i++){//elimina elementos de la cola
-        delete cola[i];
-    }
+    // for (int i = 0; i < capacidad; i++){//elimina elementos de la cola
+    //     delete cola[i];
+    // }
+    delete this->cola;
     //Se vuelve a restaurar la cola a como estaba al comienzo
     size = 0;
     capacidad = 10;
