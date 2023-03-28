664e6f8dd5a5b14127f9a79d2f293ead2ec95cde
SerlioGiron <gironserlio@gmail.com>
2023-03-28 12:10:59 -0600

    comentarios en simbolo.cpp

--- Simbolo.cpp
+++ Simbolo.cpp
@@ -5,14 +5,23 @@ Simbolo::Simbolo(char car){
 }
 Simbolo::~Simbolo(){}
 string Simbolo::toString(){
-    string toString_caracter;
-    toString_caracter += caracter;
-    return toString_caracter;
+
+    string toString_caracter; //creamos una variable de tipo string
+    toString_caracter += caracter; // a esta cadena le agregamos nuesrto char (simbolo)
+    return toString_caracter; // retornamos la cadena con el simbolo incluido
+
 }
 bool Simbolo::equals(Object* simbolo){
-    Simbolo* simboloComparacion = dynamic_cast<Simbolo*>(simbolo);
-    if (simboloComparacion->caracter == this->caracter) {
-        return true;
+
+    // Este metodo compara dos objetos de tipo Simbolo para ver si son iguales
+
+    Simbolo* simboloComparacion = dynamic_cast<Simbolo*>(simbolo); // Ya que el parametro que 
+    //recbibimos es un object, casteamos el objeto a Simbolo para podes acceder 
+    //a todas las caracteristicas de la clase Simbolo
+
+    if (simboloComparacion->caracter == this->caracter) { // comparamos los dos caracteres
+        return true; //  retornamos true si son iguales
     }
-    return false;
+    return false; // retornamos false si son distintos
+
 }
